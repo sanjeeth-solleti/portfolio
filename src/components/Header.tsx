7f1961c9cb986a66b577a8c6059f43134494e367
solleti vasava sanjeeth <vasavasanjeeth@gmail.com>
2025-06-21 11:17:06 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,7 +24,6 @@ const Header = () => {
 
   const navItems = [
     { label: 'Home', id: 'hero' },
-    { label: 'About', id: 'about' },
     { label: 'Skills', id: 'skills' },
     { label: 'Experience', id: 'experience' },
     { label: 'Projects', id: 'projects' },
