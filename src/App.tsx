68a243dc5b05b32f92d1aff533b40c4fffbc6614
solleti vasava sanjeeth <vasavasanjeeth@gmail.com>
2025-07-12 18:33:46 +0530

    Rewind Certifications changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Skills from './components/Skills';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Education from './components/Education';
-import Certifications from './components/Certifications';
 import Contact from './components/Contact';
 import CyberBackground from './components/CyberBackground';
 
@@ -26,7 +25,6 @@ function App() {
         <Experience />
         <Projects />
         <Education />
-        <Certifications />
         <Contact />
       </div>
     </div>
